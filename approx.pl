50076a765e7d899070d81bd9ecacbbee9a411568
Christian Theil Have <cth@ruc.dk>
2009-08-12 16:53:59 +0200

    first attempt

---
+++ approx.pl
@@ -0,0 +1,76 @@
+:- use_module(library(chr)).
+
+:- chr_constraint rule/2, rule/3, prime/2, terminal/1, nonterminal/1, terminal_candidate/1.
+
+init_grammar :-
+%	rule(s, [e]),
+%	rule(e, [e,'+',t]),
+%	rule(e, [t]),
+%	rule(t, [t, '*', f]),
+%	rule(t, [f]),
+	rule(f, ['(',f,')']),
+	rule(f,[a]).
+	
+new_nonterminal(N,NPrime) :-
+	atom_concat(N, '_prime',NPrime).
+	
+rule(A,B) <=> rule(A,B,original).
+
+rule(A,B,C) \ rule(A,B,C) <=> true.
+nonterminal(N) \ nonterminal(N) <=> true.
+prime(N,NP) \ prime(N,NP) <=> true.
+
+rule(_,List) <=> terminal_candidate(List).
+
+terminal_candidate([]) <=> true.
+terminal_candidate([T|Rest]) <=> terminal(T), terminal_candidate(Rest).
+
+nonterminal(X) \ terminal(X) <=> true.
+
+rule(N,_,_) ==> nonterminal(N).
+
+% (only) Original nonterminals are primed
+nonterminal(N) ==> not(new_nonterminal(_,N)) | new_nonterminal(N,NPrime), prime(N,NPrime).
+
+% Add epsilon rule for each primed nonterminal
+prime(_,N) ==> rule(N, epsilon, transformed).
+
+prime(N,NPrime) \ rule(N,RHS, original) <=>
+	append(RHS,[NPrime],RHSTransformed),
+	rule(N,RHSTransformed,transformed).
+
+%% Base case:
+rule(N,[N1], transformed), prime(N,NPrime), prime(_,N1) ==>
+	rule(NPrime, N1, transformed).
+
+% Recursion:
+terminal(T), nonterminal(N1), prime(N1,N1Prime) \ rule(N,[T,N1,R|Rest],transformed) <=>
+	rule(N,[T,N1],tranformed),
+	rule(N1Prime,[R|Rest],transformed).
+	
+terminal(T), nonterminal(N1), prime(N1,N1Prime) \ rule(N,[N1,T|Rest],transformed) <=>
+	rule(N,[N1],transformed),
+	rule(N1Prime, [T|Rest], transformed).
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
